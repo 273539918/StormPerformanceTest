168cb80ccc2130c17277c7c83897f63c74802f7b
藏红 <canghong.ch@alibaba-inc.com>
2016-11-17 13:18:19 +0800

    Storm性能测试

---
+++ ThroughputTest.java
@@ -0,0 +1,136 @@
+package performance.test;
+
+import org.apache.storm.Config;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * Created by chenhong on 16/11/14.
+ */
+public class ThroughputTest {
+    public static class GenSpout extends BaseRichSpout {
+        private static final Character[] CHARS = new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+        SpoutOutputCollector _collector;
+        int _size;
+        Random _rand;
+        String _id;
+        String _val;
+
+        public GenSpout(int size) {
+            _size = size;
+        }
+
+
+        public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+            _collector = collector;
+            _rand = new Random();
+            _id = randString(5);
+            _val = randString(_size);
+        }
+
+
+        public void nextTuple() {
+            _collector.emit(new Values(_id, _val));
+
+        }
+
+        private String randString(int size) {
+            StringBuffer buf = new StringBuffer();
+            for(int i=0; i<size; i++) {
+                buf.append(CHARS[_rand.nextInt(CHARS.length)]);
+            }
+            return buf.toString();
+        }
+
+
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "item"));
+        }
+    }
+
+    public static class IdentityBolt extends BaseBasicBolt {
+
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("id", "item"));
+        }
+
+
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            collector.emit(tuple.getValues());
+        }
+    }
+
+    public static class CountBolt extends BaseBasicBolt {
+        int _count;
+
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("count"));
+        }
+
+
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+            _count+=1;
+            collector.emit(new Values(_count));
+        }
+    }
+
+    public static class AckBolt extends BaseBasicBolt {
+
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        }
+
+
+        public void execute(Tuple tuple, BasicOutputCollector collector) {
+        }
+    }
+
+
+    //storm jar storm-benchmark-0.0.1-SNAPSHOT-standalone.jar storm.benchmark.ThroughputTest demo 100 8 8 8 10000
+    public static void main(String[] args) throws Exception {
+        int size = Integer.parseInt(args[1]);
+        int workers = Integer.parseInt(args[2]);
+        int spout = Integer.parseInt(args[3]);
+        int bolt = Integer.parseInt(args[4]);
+        int maxPending = Integer.parseInt(args[5]);
+
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("spout", new GenSpout(size), spout);
+//        builder.setBolt("count", new CountBolt(), bolt)
+//                .fieldsGrouping("bolt", new Fields("id"));
+//        builder.setBolt("bolt", new IdentityBolt(), bolt)
+//                .shuffleGrouping("spout");
+        builder.setBolt("bolt2", new AckBolt(), bolt)
+                .shuffleGrouping("spout");
+//        builder.setBolt("count2", new CountBolt(), bolt)
+//                .fieldsGrouping("bolt2", new Fields("id"));
+
+        Config conf = new Config();
+        conf.setNumWorkers(workers);
+        //conf.setMaxSpoutPending(maxPending);
+        conf.setNumAckers(0);
+        conf.setStatsSampleRate(0.0001);
+        //topology.executor.receive.buffer.size: 8192 #batched
+        //topology.executor.send.buffer.size: 8192 #individual messages
+        //topology.transfer.buffer.size: 1024 # batched
+
+        //conf.put("topology.executor.send.buffer.size", 1024);
+        //conf.put("topology.transfer.buffer.size", 8);
+        //conf.put("topology.receiver.buffer.size", 8);
+        //conf.put(Config.TOPOLOGY_WORKER_CHILDOPTS, "-Xdebug -Xrunjdwp:transport=dt_socket,address=1%ID%,server=y,suspend=n");
+
+        StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+    }
+}
---
+++ WordCountTopology.java
@@ -0,0 +1,143 @@
+package ch.main;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
+import backtype.storm.spout.SchemeAsMultiScheme;
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import storm.kafka.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by chenhong on 16/11/17.
+ */
+public class WordCountTopology {
+
+
+    public static class KafkaWordSplitter extends BaseRichBolt{
+        // private static final Log LOG = LogFactory.getLog(KafkaWordSplitter.class);
+        private static final Logger LOG = LoggerFactory.getLogger(KafkaWordSplitter.class);
+        private static final long serialVersionUID = 1L;
+        private OutputCollector collector;
+
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+            this.collector = collector;
+        }
+
+        @Override
+        public void execute(Tuple input) {
+            String line = input.getString(0);
+            LOG.info("RECE[kafka -> splitter] "+line);
+            String[] words = line.split("\\s+");
+            for(String word : words){
+                LOG.info("EMIT[splitter -> counter] "+word);
+                collector.emit(input,new Values(word,1));
+            }
+            collector.ack(input);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word","count"));
+        }
+    }
+
+    public static class WordCounter extends BaseRichBolt {
+        // private static final Log LOG = LogFactory.getLog(WordCounter.class);
+        private static final Logger LOG = LoggerFactory.getLogger(WordCounter.class);
+        private static final long serialVersionUID =1L;
+        private OutputCollector collector;
+        private Map<String,AtomicInteger> counterMap;
+
+        @Override
+        public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+            this.collector=collector;
+            this.counterMap = new HashMap<String,AtomicInteger>();
+        }
+
+        @Override
+        public void execute(Tuple input) {
+            String word = input.getString(0);
+            int count = input.getInteger(1);
+            LOG.info("RECE[splitter -> counter] "+word+" : "+count);
+            AtomicInteger ai = this.counterMap.get(word);
+            if(ai==null){
+                ai= new AtomicInteger();
+                this.counterMap.put(word,ai);
+            }
+            ai.addAndGet(count);
+            collector.ack(input);
+            LOG.info("CHECK statistics map: "+this.counterMap);
+        }
+
+        @Override
+        public void declareOutputFields(OutputFieldsDeclarer declarer) {
+            declarer.declare(new Fields("word","count"));
+        }
+
+        @Override
+        public void cleanup() {
+            LOG.info("The final result:");
+            Iterator<Map.Entry<String,AtomicInteger>> iter = this.counterMap.entrySet().iterator();
+            while(iter.hasNext()){
+                Map.Entry<String,AtomicInteger> entry =iter.next();
+                LOG.info(entry.getKey()+"\t:\t"+entry.getValue().get());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws AlreadyAliveException,InvalidTopologyException,InterruptedException{
+        String zks =Config.clusterAddress  ; //"10.101.227.20:2181,10.101.226.226:2181,10.101.226.96:2181";
+        String topic =Config.kafkaTopic;//"test-topic5";
+        String zkRoot =Config.zkRoot;//"/kafka" ;
+        String id ="word"; // 读取的status会被存在，/zkRoot/id下面，所以id类似consumer group
+
+        BrokerHosts brokerHosts = new ZkHosts(zks,"/kafka/brokers");
+        SpoutConfig spoutConf = new SpoutConfig(brokerHosts,topic,zkRoot,id);
+        spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
+        spoutConf.forceFromStart = false;
+        // spoutConf.zkServers= Arrays.asList(new String[]{"10.101.227.20","10.101.226.226","10.101.226.96"});
+        spoutConf.zkServers= Arrays.asList(new String[]{Config.clusterIP});
+        spoutConf.zkPort=2181;
+
+        TopologyBuilder  builder = new TopologyBuilder();
+        builder.setSpout("kafka-reader", new KafkaSpout(spoutConf), 5); //// Kafka我们创建了一个5分区的Topic，这里并行度设置为5
+        builder.setBolt("word-splitter",new KafkaWordSplitter(),2).shuffleGrouping("kafka-reader");
+        builder.setBolt("word-counter",new WordCounter() ).fieldsGrouping("word-splitter",new Fields("word"));
+        builder.setBolt("persistence-bolt",new PersistenceBolt()).globalGrouping("word-count");
+
+
+        Config config = new Config();
+        String name = MyKafkaTopology.class.getSimpleName();
+        if(args !=null && args.length>0 ){
+            //config.put(Config.NIMBUS_HOST,args[0]);
+            config.setNumWorkers(3);
+            StormSubmitter.submitTopology(name,config,builder.createTopology());
+        }else{
+            config.setMaxTaskParallelism(3);
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology(name,config,builder.createTopology());
+            Thread.sleep(60000);
+            cluster.shutdown();
+        }
+    }
+
+}
